d4fdaa9e569a7263238bc78ced55cc8c69a3834f
Tane Morgan <464864+tanem@users.noreply.github.com>
2020-02-10 13:10:54 +1300

    Tidy code style

--- for/repeat.go
+++ for/repeat.go
@@ -5,8 +5,10 @@ const repeatCount = 5
 // Repeat returns character repeated 5 times.
 func Repeat(character string) string {
 	var repeated string
+
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
+
 	return repeated
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-
 	assertCorrectMessage := func(t *testing.T, got, want string) {
 		t.Helper()
+
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
@@ -14,25 +14,28 @@ func TestHello(t *testing.T) {
 	t.Run("to a person", func(t *testing.T) {
 		got := Hello("Chris", "")
 		want := "Hello, Chris"
+
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World"
+
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("in Spanish", func(t *testing.T) {
 		got := Hello("Elodie", "Spanish")
 		want := "Hola, Elodie"
+
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("in French", func(t *testing.T) {
 		got := Hello("Lauren", "French")
 		want := "Bonjour, Lauren"
+
 		assertCorrectMessage(t, got, want)
 	})
-
 }
