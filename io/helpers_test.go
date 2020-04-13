3244111b1b165baaf7d8476db43dac517ab4db10
Tane Morgan <464864+tanem@users.noreply.github.com>
2020-04-13 13:51:55 +1200

    Add io

---
+++ io/helpers_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func createTempFile(t *testing.T, initialData string) (*os.File, func()) {
+	t.Helper()
+
+	tmpfile, err := ioutil.TempFile("", "db")
+
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
+}
