08b1363a2a41a3078e374d43a775acb20517a01e
Tane Morgan <464864+tanem@users.noreply.github.com>
2020-04-17 15:02:37 +1200

    Add websockets

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tanem/learn-go-with-tests
 
 go 1.14
+
+require github.com/gorilla/websocket v1.4.2
---
+++ websockets/tape_test.go
@@ -0,0 +1,27 @@
+package poker_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	poker "github.com/tanem/learn-go-with-tests/websockets"
+)
+
+func TestTape_Write(t *testing.T) {
+	file, clean := createTempFile(t, "12345")
+	defer clean()
+
+	tape := &poker.Tape{File: file}
+
+	tape.Write([]byte("abc"))
+
+	file.Seek(0, 0)
+	newFileContents, _ := ioutil.ReadAll(file)
+
+	got := string(newFileContents)
+	want := "abc"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
